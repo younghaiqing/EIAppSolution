878de44e7d359a760ce193395d6f74b3d74cf919
515632356@qq.com <wu515632356>
2016-03-23 14:53:08 +0800

    框架搭建

---
+++ EIApp.WebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EIApp.WebSite.MvcApplication" Language="C#" %>
---
+++ EIApp.WebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EIApp.WebSite.Startup))]
+namespace EIApp.WebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
