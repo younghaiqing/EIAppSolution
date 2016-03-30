27ddf5d46e8c3c4662b7feef098ca0bac0f632bf
515632356@qq.com <wu515632356>
2016-03-30 16:47:00 +0800

    添加easyUI

---
+++ EIApp.WebSite/Controllers/LoginController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EIApp.WebSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UserLogin(string name)
+        {
+            if (name != "")
+            {
+                return Redirect("/Login/Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ActionResult UserLogin()
+        {
+            return View();
+        }
+    }
+}
