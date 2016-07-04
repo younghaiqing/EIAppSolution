3b10ef5883c15b5feb9a72bb89eefb8ed4f80ee9
515632356@qq.com <wu515632356>
2016-07-04 11:13:58 +0800

    log4net发邮件添加

--- ConsoleLog/Program.cs
+++ ConsoleLog/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleLog
     {
         private static void Main(string[] args)
         {
-            LoggerUtils.Log4ILog log = new LoggerUtils.Log4Nlog() ;//new LoggerUtils.Log4Log4Net();
+            LoggerUtils.Log4ILog log = new LoggerUtils.Log4Log4Net();//new LoggerUtils.Log4Nlog() ;
             log.WriteDebugLog("test");
             Console.WriteLine("日志记录完毕。");
             Console.Read();
